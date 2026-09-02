59836fa20da54eae89dd4c4913ae52f0c6bd9aca
faresw13 <fares.srtt@gmail.com>
2026-09-02 08:26:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "AI Agent is running"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json(silent=True)
+
+    print("SALLA WEBHOOK:")
+    print(data)
+
+    return jsonify({
+        "success": True
+    }), 200
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({
+        "status": "ok"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
